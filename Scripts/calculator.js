fac7a791a46080ce2aa520528832901b068f8b9d
Andrew Nicklen <andrew.nicklen@sage.com>
2019-07-17 12:27:00 +0100

    reset button working

--- Scripts/calculator.js
+++ Scripts/calculator.js
@@ -62,8 +62,8 @@ equalsButton.addEventListener('click', (e) => {
     document.getElementById('screen').innerHTML = operate(calculationArray[calculationArray.length - 2],calculationArray[calculationArray.length - 1],calculationArray[calculationArray.length - 3]);
 })
 
-//pressing = operates them, produces a result and puts that result in [0], and removes [1] and [2]
-//if the user presses an operator without typing num1, it uses the previous total in [0]
-//the approach above allows the user to do long sums, i.e. 2+2=4*3 
-//need to think about how we do long sums without = i.e. 2*3+4-5
-//one way could be to store subsequent numbers and operators in the array from [3]
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', (e) => {
+    calculationArray = [];
+    document.getElementById('screen').innerHTML = "";
+})
