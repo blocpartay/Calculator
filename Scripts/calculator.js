9cf14512c0ebc577b70c32a8197dea66f6ab5a3e
Andrew Nicklen <andrew.nicklen@sage.com>
2019-07-18 09:15:02 +0100

    variable names changed

--- Scripts/calculator.js
+++ Scripts/calculator.js
@@ -1,5 +1,5 @@
-let calculationArray = [];
-let tempCalculationTotal = 0;
+let calcArray = [];
+let runningTotal = 0;
 
 function add (num1, num2) {
     return (parseInt(num1, 10) + parseInt(num2, 10));
@@ -37,13 +37,14 @@ const numberButtons = Array.from(document.querySelectorAll('.numberButton'));
 numberButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
         currentScreen = document.getElementById('screen').innerHTML;
+        
         if (currentScreen == '+' || currentScreen == '-' || currentScreen == 'x' || currentScreen == '/') {
             document.getElementById('screen').innerHTML = button.innerHTML;
-            if (calculationArray.length < 3) {
-                tempCalculationTotal = operate(calculationArray[calculationArray.length - 2],calculationArray[calculationArray.length - 1],button.innerHTML);
+            if (calcArray.length < 3) {
+                runningTotal = operate(calcArray[0],calcArray[1],button.innerHTML);
             }
             else {
-                tempCalculationTotal = operate(tempCalculationTotal,calculationArray[calculationArray.length - 1],button.innerHTML);
+                runningTotal = operate(runningTotal,calcArray[calcArray.length - 1],button.innerHTML);
             }    
         }
         else {
@@ -56,39 +57,24 @@ const operatorButtons = Array.from(document.querySelectorAll('.operatorButton'))
 operatorButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
         currentScreen = document.getElementById('screen').innerHTML;
-        calculationArray.push(currentScreen);
+        calcArray.push(currentScreen);
         if (currentScreen == '+' || currentScreen == '-' || currentScreen == 'x' || currentScreen == '/') {
             alert('Please enter a number');
         }
         else {
         document.getElementById('screen').innerHTML = button.innerHTML;
-        calculationArray.push(button.innerHTML);
+        calcArray.push(button.innerHTML);
         }
     })
 })
 
 const equalsButton = document.getElementById('equals');
 equalsButton.addEventListener('click', (e) => {
-    document.getElementById('screen').innerHTML = tempCalculationTotal;
-    /*
-    currentScreen = document.getElementById('screen').innerHTML;
-    calculationArray.push(currentScreen);
-
-    if (calculationArray.length >3) {
-        console.log(calculationArray);
-        console.log(tempCalculationTotal);
-        console.log(calculationArray[calculationArray.length - 2]);
-        console.log(calculationArray[calculationArray.length - 1]);
-        document.getElementById('screen').innerHTML = operate(tempCalculationTotal,calculationArray[calculationArray.length - 2],calculationArray[calculationArray.length - 1]);   
-    }
-    else {
-        document.getElementById('screen').innerHTML = operate(calculationArray[calculationArray.length - 3],calculationArray[calculationArray.length - 2],calculationArray[calculationArray.length - 1]);
-    }
-    */
+    document.getElementById('screen').innerHTML = runningTotal;
 })
 
 const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', (e) => {
-    calculationArray = [];
+    calcArray = [];
     document.getElementById('screen').innerHTML = "";
 })
